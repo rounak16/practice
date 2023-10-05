a61c8eceea4049b0e65e6f6986597d3704f203a2
rounak16 <130536808+rounak16@users.noreply.github.com>
2023-10-05 14:43:38 +0530

    Time: 8 ms (13.56%), Space: 42.6 MB (65.03%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        StringBuilder s = new StringBuilder(String.valueOf(x));
+        s.reverse();
+       
+        return s.toString().equals(x + "");
+    }
+}
